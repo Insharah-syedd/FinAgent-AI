61e1cdb56d523779dbb1cf5a1d76c97a1e93ce5a
Insharah Syed <insharahsyedd@gmail.com>
2026-08-25 21:15:25 +0500

    Fix syntax in sys.path

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 import streamlit as st
 import pandas as pd
 from ai_finance_intelligence.crew import AiFinanceIntelligence
